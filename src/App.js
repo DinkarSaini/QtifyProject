d2a0599d132e3b2d313a882073a29b8e68459768
DinkarSaini <dinkarsaini2001@gmail.com>
2023-08-21 10:36:50 +0530

    card-qtify-takehome

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import { fetchTopAlbums } from "./api/api";
+import { useEffect , useState } from "react";
+import Card from "./components/Card/Card";
+import { data } from "./mockData/topAlbum"
 
 function App() {
+  const [topAlbum , setTopAlbum] = useState(data);
+  const topAlbumData = async()=>{
+    try{
+      const data = await fetchTopAlbums();
+      setTopAlbum(data);
+    }catch(err){
+      console.error(err)
+    }
+   
+  }
+  useEffect(()=>{
+  topAlbumData();
+  },[])
   return (
   <>
     <Navbar />
     <Hero />
+    <Card data = {topAlbum[0]} type="album"/>
   </>
   );
 }
